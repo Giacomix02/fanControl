c97086ef549e481670b784b18ecff6e8bc99ddaa
Giacomix02 <paoloccigiacomo@gmail.com>
2024-06-22 12:13:33 +0200

    the app now runs as administrator and doesn't ask for permission on every user action

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,18 @@
 fn main() {
-  tauri_build::build()
+  //tauri_build::build()
+  let mut windows = tauri_build::WindowsAttributes::new();
+windows = windows.app_manifest(r#"
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+  <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+        <requestedPrivileges>
+            <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+        </requestedPrivileges>
+    </security>
+  </trustInfo>
+</assembly>
+"#);
+tauri_build::try_build(
+  tauri_build::Attributes::new().windows_attributes(windows)
+).expect("failed to run build script");
 }
